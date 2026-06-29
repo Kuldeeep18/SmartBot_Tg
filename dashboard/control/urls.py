42bdd0c67c2b11f9de7206bd68f3ee7c2effc4d7
Sandip <mrsandipgodhani@gmail.com>
2026-06-30 02:52:24 +0530

    UI upgraded v1.0

--- dashboard/control/urls.py
+++ dashboard/control/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.landing, name='landing'),
+    path('login/', views.login_view, name='login'),
+    path('dashboard/', views.home, name='home'),
     path('api/bot/launch', views.launch_bot_api, name='launch_bot_api'),
     path('api/bot/status', views.status_bot_api, name='status_bot_api'),
     path('api/log_error', views.log_error, name='log_error'),
